e8ef00990e7ed8b0d9dfab3e980886fe8ed7f060
Linus Widmer <lwidmer@4b0c17.42berlin.de>
2022-12-22 10:27:01 +0100

    norminette

--- sources_ft_printf/ft_print_address_hex.c
+++ sources_ft_printf/ft_print_address_hex.c
@@ -1,17 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_address_hex.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lwidmer <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/22 09:36:32 by lwidmer           #+#    #+#             */
+/*   Updated: 2022/12/22 09:40:45 by lwidmer          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 #include "ft_printf.h"
 
-static void print_address(unsigned long long address)
+static void	print_address(unsigned long long address)
 {
+	char	print;
+	char	base[17];
 
-	char print;
-  char base[16] = "0123456789abcdef";
-	if(address < 16)
+	base = ft_strncpy(base, "0123456789abcdef");
+	if (address < 16)
 	{
 		print = base[address % 16];
 		write(1, &print, 1);
-	} 
-	else 
+	}
+	else
 	{
 		print_address(address / 16);
 		print = base[address % 16];
@@ -19,25 +32,25 @@ static void print_address(unsigned long long address)
 	}
 }
 
-static unsigned int length_address(unsigned long long address)
+static unsigned int	length_address(unsigned long long address)
 {
-  unsigned int len;
+	unsigned int	len;
 
-  len = 1;
-  while (address > 15)
-  {
-    len++;
-    address = address / 16;
-  }
-  return (len);
+	len = 1;
+	while (address > 15)
+	{
+		len++;
+		address = address / 16;
+	}
+	return (len);
 }
 
-unsigned int ft_print_address_hex(unsigned long long address)
+unsigned int	ft_print_address_hex(unsigned long long address)
 {
-  if(address)
-  {
-    ft_putstr_fd("0x", 1);
-    print_address(address);
-  }
-  return (length_address(address));
+	if (address)
+	{
+		ft_putstr_fd("0x", 1);
+		print_address(address);
+	}
+	return (length_address(address));
 }
